b16c569b15f026a02d5c9ef2ec16e0e36218ef55
Dan Saunders <danjsaund@gmail.com>
2025-11-27 20:57:02 -0500

    Restore default hyperparameters

--- full_trained_egg.c
+++ full_trained_egg.c
@@ -40,11 +40,11 @@
 
 // --- Configuration [cite: 275, 277, 288] ---
 #define VOCAB_SIZE 256        // Byte-level tokenization
-#define HIDDEN_DIM 128        // Model width
-#define N_LAYERS 2            // Number of layers
-#define SEQ_LEN 512            // Sequence length for BPTT (truncated)
-#define POPULATION_SIZE 16    // Number of perturbations per step
-#define BATCH_SIZE 2          // Parallel streams
+#define HIDDEN_DIM 512        // Model width
+#define N_LAYERS 4            // Number of layers
+#define SEQ_LEN 4096            // Sequence length for BPTT (truncated)
+#define POPULATION_SIZE 128    // Number of perturbations per step
+#define BATCH_SIZE 8          // Parallel streams
 #define FIXED_POINT 4         // 4 bits for fractional part
 #define SIGMA_SHIFT 4         // Noise scale (bitwise shift)
 #define UPDATE_THRESHOLD 160  // Votes needed to flip a weight [cite: 1023]
